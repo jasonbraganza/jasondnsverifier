c3a5e6b63f100c09310e1dcaadf71b5a8236be29
Mario Jason Braganza <jason@janusworx.com>
2020-08-21 12:06:47 +0530

    wip
    Signed-off-by: Mario Jason Braganza <jason@janusworx.com>

--- jdv.py
+++ jdv.py
@@ -11,21 +11,18 @@ import requests
 
 
 def main():
-    list_of_sites = []
     get_list_of_sites()
     compare_list_with_providers(list_of_sites)
 
 
 def get_list_of_sites():
-
     try:
+        list_of_sites = []
         if sys.argv[1]:
-
             with open(f"{sys.argv[1]}") as list_to_process:
                 for each_site in list_to_process:
-                    # print(each_site)
-                    list_of_sites.append(each_site)
-
+                    list_of_sites.append(each_site.strip().lower())
+            return list_of_sites
     except:
         print("Please give a text file of domains, one per line, to process.")
 
