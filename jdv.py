a5b7be9e14731070f62c5460f5a4d843f573acbc
Mario Jason Braganza <jason@janusworx.com>
2020-08-21 12:59:23 +0530

    done!
    Signed-off-by: Mario Jason Braganza <jason@janusworx.com>

--- jdv.py
+++ jdv.py
@@ -11,24 +11,57 @@ import requests
 
 
 def main():
-    get_list_of_sites()
+    list_of_sites = get_list_of_sites()
     compare_list_with_providers(list_of_sites)
 
 
 def get_list_of_sites():
     try:
-        list_of_sites = []
         if sys.argv[1]:
             with open(f"{sys.argv[1]}") as list_to_process:
-                for each_site in list_to_process:
-                    list_of_sites.append(each_site.strip().lower())
-            return list_of_sites
+                site_list = [each_site.strip().lower() for each_site in list_to_process]
+                return site_list
     except:
         print("Please give a text file of domains, one per line, to process.")
 
 
 def compare_list_with_providers(some_list_to_process):
-    print(some_list_to_process)
+    for site in some_list_to_process:
+        ip_local = local_lookup(site)
+        ip_from_google = google_lookup(site)
+        ip_from_cloudflare = cloudflare_lookup(site)
+        if ip_local == ip_from_google and ip_local == ip_from_cloudflare:
+            pass
+        elif ip_local == ip_from_google and not ip_local == ip_from_cloudflare:
+            print(
+                f"{site}: Local dns lookup matches with Google, but not Cloudflare.\n"
+            )
+        elif ip_local == ip_from_cloudflare and not ip_local == ip_from_google:
+            print(
+                f"{site}: Local dns lookup matches with Cloudflare, but not Google.\n"
+            )
+        else:
+            print(
+                f"{site}: Local dns lookup does not match with either Google or Cloudflare.\n"
+            )
+
+
+def local_lookup(some_site):
+    site_ip = socket.gethostbyname(some_site).strip()
+    return site_ip
+
+
+def google_lookup(some_site):
+    google_lookup = requests.get(f"https://dns.google/resolve?name={some_site}")
+    site_ip = google_lookup.json()["Answer"][0]["data"].strip()
+    return site_ip
+
+
+def cloudflare_lookup(some_site):
+    cloudflare_url = f"https://cloudflare-dns.com/dns-query?name={some_site}&type=A&ct=application/dns-json"
+    cloudflare_lookup = requests.get(cloudflare_url)
+    site_ip = cloudflare_lookup.json()["Answer"][0]["data"].strip()
+    return site_ip
 
 
 if __name__ == "__main__":
